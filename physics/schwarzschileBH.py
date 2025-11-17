628ac55c51d597efcc0dee4a4c21553e0118c6b5
1nonly <tobiasbraymen@gmail.com>
2025-11-17 19:26:53 +0000

    Added radius calculation for schwarzschildBH, updated trajectory

--- physics/schwarzschileBH.py
+++ physics/schwarzschileBH.py
@@ -22,3 +22,17 @@ def effectiveDensity(mass):
     """
     density = (1 / (mass ** 2)) * ((3 * (const.c ** 6)) / (32 * const.PI * (const.G ** 3)))
     return density
+
+def radius(mass):
+    """
+    Calculates the Schwarzschild radius of a black hole.
+    Args:
+        mass (float): Mass of black hole in kg
+    
+    Returns:
+        float: Schwarzschild radius in meters
+    """
+    radius = (2 * const.G * mass) / (const.c ** 2)
+    return radius
+print(radius(1.98900e30))
+print(lifeTime(1.989e30))
--- physics/trajectory.py
+++ physics/trajectory.py
@@ -2,14 +2,23 @@ import math
 import matplotlib.pyplot as plt
 
 g = 9.807 #m/s^2
-def calculateDistance(Vi, a, h):
+def calcDistanceNoAirFric(Vi, a, h):
+    """
+    Calculates the distance traveled by a projectile without air friction.
+    Args:
+        Vi (float): Initial velocity in m/s
+        a (float): Angle of launch in degrees
+        h (float): Inital height in meters
+    Returns: 
+        float: Distance traveled in meters
+    """
     Viy = Vi * (math.sin(math.radians(a)))
     Vix = Vi * (math.cos(math.radians(a)))
     totalTime = ((Viy + (((Viy ** 2) + (2 * (g * h))) ** 0.5)) / g)
     distance = Vix * totalTime
     return distance
 
-def calculateTotalTime(Vi, a, h):
+def calcTimeNoAirFric(Vi, a, h):
     Viy = Vi * (math.sin(math.radians(a)))
     totalTime = ((Viy + (((Viy ** 2) + (2 * (g * h))) ** 0.5)) / g)
     return totalTime
@@ -18,24 +27,26 @@ v = float(input("Enter initial velocity (m/s): "))
 a = int(input("Enter angle (degrees): "))
 h = float(input("Enter initial height (m): "))
 
-print(f"Distance: {calculateDistance(v, a, h)} m")
-print(f"Total Time: {calculateTotalTime(v, a, h)} s")
+
+def graphTra(distance):
+    """
+    Graphs the trajectory of a projectile without air friction.
+    Args:
+        distance (float): Distance traveled in meters
+    """
+    x = []
+    y = []
+    i = 0
+    while ((v * (math.cos(math.radians(a)))) * i) <= distance:
+        x.append((v * (math.cos(math.radians(a)))) * i)
+        y.append(h + ((v * (math.sin(math.radians(a)))) * i) - ((g * (i ** 2)) / 2))
+        i += 0.01
 
 
-x = []
-y = []
-dist = calculateDistance(v, a, h)
-i = 0
-while ((v * (math.cos(math.radians(a)))) * i) <= dist:
-    x.append((v * (math.cos(math.radians(a)))) * i)
-    y.append(h + ((v * (math.sin(math.radians(a)))) * i) - ((g * (i ** 2)) / 2))
-    i += 0.01
-
-
-plt.ylim((0), (max(y) * 1.1))
-plt.xlim((0), (max(x) * 1.1))
-plt.plot(x, y)
-plt.xlabel("Distance (m)")
-plt.ylabel("Height (m)")
-plt.title("Distance (x) vs Height (y) trajectory")
-plt.show()
+    plt.ylim((0), (max(y) * 1.1))
+    plt.xlim((0), (max(x) * 1.1))
+    plt.plot(x, y)
+    plt.xlabel("Distance (m)")
+    plt.ylabel("Height (m)")
+    plt.title("Distance (x) vs Height (y) trajectory")
+    plt.show()
